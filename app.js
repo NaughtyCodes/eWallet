ce7dbac96af08e07f1e75fc82b5f82581df78b08
krishnan <krish_nan@outlook.com>
2017-06-23 01:46:52 +0530

    ui tree grid

--- app.js
+++ app.js
@@ -1,200 +1,155 @@
- var data = [{
-			"year":2016,
-			"impl":"key1",
-            "id": 1,
-            "parentId": 1,
-            "name": "Loc2 child"
-          },{
-			"year":2016,
-			"impl":"key1",
-            "id": 2,
-            "parentId": 1,
-            "name": "Loc2 child"
-          },{
-			"year":2016,
-			"impl":"key2",
-            "id": 3,
-            "parentId": 2,
-            "name": "Loc2 child"
-          },{
-			"year":2017,
-			"impl":"key1",
-            "id": 4,
-            "parentId": 1,
-            "name": "Loc2 child"
-          },{
-			"year":2017,
-			"impl":"key1",
-            "id": 5,
-            "parentId": 1,
-            "name": "Loc2 child"
-          }]
-  
-  
-  function formatData(){
-		
-		year = {};
-		var implObj = {};
-		var implArray = [];
-		var fdata = [];
-		year['children'] = [];
-		implObj['children'] = [];
-		
-		var tYear = "";
-		var tImpl = "";
-		var implChange = false;
-		init = true;
-		angular.forEach(data, function(value, key){
-		
-					
-			if(tYear === "" || tYear === value.year){
-				//--------------------------------------------
-				year[Object.keys(value)[0]] = value.year;
-				tYear = value.year;
-				if(tImpl === "" || tImpl === value.impl){
-					if(implObj === undefined)
-					{
-					implObj = {};
-					implObj['children'] = [];
-					}
-					implObj.impl = value.impl;
-					implObj['children'].push(value);
-					tImpl = value.impl;
-					implChange = false;
-				} else {
-					implChange = true;
-					year['children'].push(implObj);
-					var implObj = {};
-					implObj['children'] = [];
-					implObj[Object.keys(value)[1]] = value.impl;
-					implObj['children'].push(value);
-					tImpl = value.impl;
-				}
-				//--------------------------------------------
-			} else {
-				if(!implChange){
-					year['children'].push(implObj);
-				} else {
-					fdata.push(year);
-				}
-					fdata.push(year);
-					year = {};
-					year['children'] = [];
-					tYear = value.year;
-					
-			//--------------------------------------------
-				year[Object.keys(value)[0]] = value.year;
-				tYear = value.year;
-				if(tImpl === "" || tImpl === value.impl){
-					implObj.impl = value.impl;
-					implObj["children"].push(value);
-					tImpl = value.impl;
-					implChange = false;
-				} else {
-					implChange = true;
-					year['children'].push(implObj);
-					var implObj = {};
-					implObj['children'] = [];
-					implObj[Object.keys(value)[1]] = value.impl;
-					implObj['children'].push(value);
-					tImpl = value.impl;
-				}
-				//--------------------------------------------
-				
-			} 
+var app = angular.module('app', [ 'ngAnimate', 'ngTouch', 'ui.grid',
+		'ui.grid.treeView', 'ui.grid.pagination' ]);
+
+app.controller('MainCtrl', [
+		'$scope',
+		'$http',
+		'$interval',
+		'uiGridTreeViewConstants',
+		function($scope, $http, $interval, uiGridTreeViewConstants) {
+			$scope.gridOptions = {
+				enableSorting : true,
+				enableFiltering : true,
+				showTreeExpandNoChildren : true,
+				paginationPageSizes : [ 5, 10 ],
+				paginationPageSize : 5,
+				columnDefs : [ {
+					name : 'id',
+					width : '30%'
+				}, {
+					name : 'parentId',
+					width : '20%'
+				}, {
+					name : 'name',
+					width : '20%'
+				}, ]
+			};
+
+			//----------------------------------------------------------------------------------------------------
 			
-			console.dir(fdata);
-		});
-		
-  }
+				var d = [
+				         {"name":"name1","type":"type1","age":"1","city":"city1","address":"address2"},
+				         {"name":"name1","type":"type2","age":"1","city":"city1","address":"address2"},
+				         {"name":"name1","type":"type3","age":"1","city":"city1","address":"address2"},
+				         {"name":"name1","type":"type1","age":"1","city":"city1","address":"address2"},
+				         {"name":"name2","type":"type2","age":"2","city":"city2","address":"address2"},
+				         {"name":"name2","type":"type2","age":"2","city":"city2","address":"address2"},
+				         {"name":"name2","type":"type2","age":"2","city":"city2","address":"address2"},
+				         {"name":"name2","type":"type1","age":"2","city":"city2","address":"address2"},
+				         {"name":"name3","type":"type1","age":"3","city":"city3","address":"address2"},
+				         {"name":"name3","type":"type3","age":"3","city":"city3","address":"address2"},
+				         {"name":"name3","type":"type3","age":"3","city":"city3","address":"address2"}
+				        ]
+				
+				//user input
+				var ui = {
+							"$$treeLevel_0":["name"],
+							"$$treeLevel_1":["type"],
+							"$$treeLevel_2":["age","city","address"]
+						 }
+				
+				function readUserInput(ui,d){
+					
+					var to = {};
+					var fd = [];
+					
+					for (var key in ui) {
+						  if (ui.hasOwnProperty(key)) {
+						    console.log(key + " -> " + ui[key]);
+						    
+						    angular.forEach(ui["$$treeLevel_0"], function(v, i){
+						    	var tv = "";
+						    	angular.forEach(d, function(dv, di){
+						    		if(tv !== dv[v]){
+						    			
+						    			if(d.length === 0){
+						    				to = {};
+							    			to.children = [];
+							    			to[v] = dv[v];
+							    			to.$$treeLevel = key.split("_")[1];
+						    			} else {
+						    				fd.push(to);
+							    			to = {};
+							    			to.children = [];
+							    			to[v] = dv[v];
+							    			to.$$treeLevel = key.split("_")[1];
+						    			}
+						    			
+						    		} else {
+						    			to.children.push(dv);
+						    		}
+						    		
+						    		if(d.length === di)
+						    			fd.push(to);
+								   
+						    	});
+						    	
+						    });
+						    
+						 }
+					}
+				}
+				
+				
+				var writeoutNode = function(childArray, currentLevel, dataArray) {
+					childArray.forEach(function(childNode) {
+						//    if ( childNode.children.length > 0 ){
+						childNode.$$treeLevel = currentLevel;
+						//    }
+						dataArray.push(childNode);
+						writeoutNode(childNode.children, currentLevel + 1,
+								dataArray);
+					});
+				};
 
-var app = angular.module('app', ['ngAnimate', 'ngTouch', 'ui.grid', 'ui.grid.treeView', 'ui.grid.pagination']);
+			//----------------------------------------------------------------------------------------------------
 
-app.controller('MainCtrl', ['$scope', '$http', '$interval', 'uiGridTreeViewConstants', function ($scope, $http, $interval, uiGridTreeViewConstants ) {
-  
- 
-  
-  formatData();
-  
-  
-  $scope.gridOptions = {
-    enableSorting: true,
-    enableFiltering: true,
-    showTreeExpandNoChildren: true,
-    paginationPageSizes: [5, 10],
-    paginationPageSize: 5,
-    columnDefs: [
-	{ name: 'year'},
-	{ name: 'impl'},
-      { name: 'id'},
-      { name: 'parentId' },
-      { name: 'name'},
-     ]
-  };
-
-var data =[{
-    "year": 2016,
-    "children": [{
-        "impl": "key1",
-        "children": [{
-			"year":2016,
-			"impl":"key1",
-            "id": 4,
-            "parentId": 2,
-            "name": "Loc2 child",
-            "children": []
-          },{
-			"year":2016,
-			"impl":"key1",
-            "id": 4,
-            "parentId": 2,
-            "name": "Loc2 child",
-            "children": []
-          }]
-      },{
-        "impl": "key2",
-        "children": [{
-			"year":2016,
-			"impl":"key2",
-            "id": 4,
-            "parentId": 2,
-            "name": "Loc2 child",
-            "children": []
-          }]
-      }]
-  },{
-    "year": 2017,
-    "children": [{
-        "impl": "key1",
-        "children": [{
-			"year":2017,
-			"impl":"key1",
-            "id": 4,
-            "parentId": 2,
-            "name": "Loc2 child",
-            "children": []
-          },{
-			"year":2017,
-			"impl":"key1",
-            "id": 4,
-            "parentId": 2,
-            "name": "Loc2 child",
-            "children": []
-          }]
-      
-      }]
-  }]
-
-var writeoutNode = function( childArray, currentLevel, dataArray ){
-  childArray.forEach( function( childNode ){
-    if ( childNode.children.length > 0 ){
-      childNode.$$treeLevel = currentLevel;
-    }
-    dataArray.push( childNode );
-    writeoutNode( childNode.children, currentLevel + 1, dataArray );
-  });
-};
-
-$scope.gridOptions.data = [];
-writeoutNode( data, 0, $scope.gridOptions.data );
-}]);
+			$scope.gridOptions.data = [
+			  {
+				"id" : 1,
+				"parentId" : 1,
+				"name" : "TopLoc1",
+				"$$treeLevel" : 0,
+				"children" : [ {
+					"id" : 2,
+					"parentId" : 1,
+					"name" : "Loc1 child",
+					"$$treeLevel" : 1,
+					"children" : [ {
+						"id" : 4,
+						"parentId" : 2,
+						"name" : "Loc2 child",
+						"$$treeLevel" : 2
+					} ]
+					
+				}, {
+					"id" : 3,
+					"parentId" : 1,
+					"name" : "Loc1 child",
+					"$$treeLevel" : 1
+				} ]
+			
+			}, {
+				"id" : 2,
+				"parentId" : 1,
+				"name" : "Loc1 child",
+				"children" : [ {
+					"id" : 4,
+					"parentId" : 2,
+					"name" : "Loc2 child",
+					"$$treeLevel" : 2
+				} ],
+				"$$treeLevel" : 1
+			}, {
+				"id" : 4,
+				"parentId" : 2,
+				"name" : "Loc2 child",
+				"$$treeLevel" : 2
+			}, {
+				"id" : 3,
+				"parentId" : 1,
+				"name" : "Loc1 child",
+				"$$treeLevel" : 1
+			}]
+			
+''		} ]);
